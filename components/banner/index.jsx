1c4faf7f1dd5668431545f0f4db63e1d74e30ac3
unknown <alyssoncolombo@outlook.com>
2022-07-01 02:43:48 -0300

    :sparkles: Created: responsive banner created

--- components/banner/index.jsx
+++ components/banner/index.jsx
@@ -1,5 +1,55 @@
 import * as React from "react";
 
+import { Box, Button, Grid } from "@mui/material";
+import Image from "next/image";
+
+import Hidden from "@material-ui/core/Hidden";
+import alysson from "../../public/img/Banner/alysson.svg";
+import { TitleBanner, TextBanner } from "../style";
+
 export default function Banner() {
-  return <div></div>;
+  return (
+    <Box
+      pt="25px"
+      sx={{
+        backgroundColor: "#F2F2F2",
+        width: "100%",
+      }}
+    >
+      <Grid container flexDirection={"row"}>
+        <Grid sx={{ width: "40%" }} container alignContent={"center"}>
+          <Image height={"400px"} src={alysson} />
+        </Grid>
+        <Grid sx={{ width: "60%" }} container alignContent={"center"}>
+          <TitleBanner>Olá, sou Alysson Colombo</TitleBanner>
+          <TextBanner>
+            Seja bem-vindo ao meu portfólio aqui apresento minhas habilidades e
+            projetos!
+          </TextBanner>
+          <Hidden only={["xs", "sm"]}>
+            <Grid container flexDirection={"row"} gap={"24px"}>
+              <Button
+                sx={{
+                  backgroundColor: "#B0009F",
+                  color: "#FFF",
+                  borderRadius: "20px",
+                }}
+              >
+                Ver Habilidades
+              </Button>
+              <Button
+                sx={{
+                  backgroundColor: "#00B0B0",
+                  color: "#FFF",
+                  borderRadius: "20px",
+                }}
+              >
+                Ver Projetos
+              </Button>
+            </Grid>
+          </Hidden>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
--- components/style.js
+++ components/style.js
@@ -5,4 +5,41 @@ export const DivMainMenu = styled.div`
   flex-direction: row;
   background-color: aliceblue;
   gap: 10px;
+  font-size: 25px;
+`;
+
+export const TitleBanner = styled.h1`
+  display: flex;
+  font-weight: 700;
+  color: #000;
+  font-size: 45px;
+  margin: 0px;
+  @media (max-width: 1250px) {
+    font-size: 40px;
+  }
+  @media (max-width: 1000px) {
+    font-size: 35px;
+  }
+  @media (max-width: 900px) {
+    font-size: 30px;
+  }
+  @media (max-width: 455px) {
+    font-size: 20px;
+  }
+`;
+
+export const TextBanner = styled.p`
+  font-size: 30px;
+  width: 700px;
+  margin: 0px 0px 15px 0px;
+
+  @media (max-width: 1250px) {
+    font-size: 25px;
+  }
+  @media (max-width: 1000px) {
+    font-size: 20px;
+  }
+  @media (max-width: 900px) {
+    font-size: 15px;
+  }
 `;
